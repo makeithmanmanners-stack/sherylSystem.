840ba9511264e4f8ffaa43dde2cd8d0d1c2e0c1c
manmen2164-max <manmen2164@gmail.com>
2026-07-28 19:41:49 +0800

    Update admin password

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python -c "import os; import django; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store_erp.settings'); django.setup(); from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
+python -c "import os; import django; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store_erp.settings'); django.setup(); from django.contrib.auth.models import User; u, _ = User.objects.get_or_create(username='admin', defaults={'email': 'admin@example.com'}); u.set_password('Sheydekertdeo051804'); u.is_superuser=True; u.is_staff=True; u.save()"
