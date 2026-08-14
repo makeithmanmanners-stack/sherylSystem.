892839bd3818e0bb560c174a75de24aec7cb1e45
manmen2164-max <manmen2164@gmail.com>
2026-08-14 14:05:12 +0800

    Configure CSRF trusted origins for Render and enhance admin bootstrap in build.sh

--- store_erp/settings.py
+++ store_erp/settings.py
@@ -40,6 +40,17 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.onrender.com',
+    'https://sheryl-system-app-s64h.onrender.com',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
+if RENDER_EXTERNAL_HOSTNAME:
+    CSRF_TRUSTED_ORIGINS.append(f'https://{RENDER_EXTERNAL_HOSTNAME}')
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # Application definition
 
